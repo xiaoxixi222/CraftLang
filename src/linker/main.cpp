c4b418a6f5ff303bd2f8cfa7b68c7bc7981f15ac
xiaoxixi222 <xiaoxixi202205@163.com>
2026-08-10 14:18:17 +0800

    refactor: 源码按编译器/链接器拆分为 src/compiler 与 src/linker，新增 craftlinker 目标

---
+++ src/linker/main.cpp
@@ -0,0 +1,11 @@
+#include <json.hpp>
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "craftlinker: CraftLang linker (under construction)\n";
+    std::cout << "Usage: craftlinker <output-dir> <input1> [input2 ...]\n";
+    return 0;
+}
